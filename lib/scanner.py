de0fc9560010dc67c5eb7ac892d7140678be5a3b
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2021-07-26 15:46:48 -0400

    Update scanner

--- lib/scanner.py
+++ lib/scanner.py
@@ -1,5 +1,11 @@
 import nmap3
 
+DEV_LIST_LOC = '../dev_list'
+
 nm = nmap3.Nmap()
 
 results = nm.nmap_list_scan(host)
+
+dev_list = open(DEV_LIST_LOC, 'w')
+dev_list.write(results)
+dev_list.close()
