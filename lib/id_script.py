9a84bf743c281b601021214e37198229efeb8d93
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2021-08-02 09:47:44 -0400

    Update main and identifier script

--- lib/id_script.py
+++ lib/id_script.py
@@ -1,8 +1,3 @@
-import json
-DEV_LIST_LOC = './dev_list.json'
-VENDORS = ['ecobee']
-
-
 class identifier:
     def __init__(self, dev_lst, ven_lst):
         self.devices = dev_lst
@@ -10,18 +5,10 @@ class identifier:
 
     def iotid(self):
         dev_list_ven = []
-        for key, value in dev_list.items():
+        for key, value in self.devices.items():
             try:
                 dev_list_ven.append({'ip': key, 'vendor': value['macaddress']['vendor']})
             except:
                 pass
         dev_list_ven = [i for i in dev_list_ven if i['vendor'] in self.vendors]
         return dev_list_ven
-
-
-f = open(DEV_LIST_LOC, 'r')
-dev_list = json.load(f)
-f.close()
-
-id1 = identifier(dev_list, VENDORS)
-print(id1.iotid())
--- lib/main.py
+++ lib/main.py
@@ -1,7 +1,11 @@
+import json
+
 from scanner import scanner
+from id_script import identifier
 
 DEV_LIST_LOC = 'dev_list.json'
 HOST = '192.168.0.0/24'
+VENDORS = ['ecobee']
 
 sc = scanner(HOST)
 nmap_dev_list = sc.nmap_list_scan()
@@ -9,3 +13,10 @@ nmap_dev_list = sc.nmap_list_scan()
 dev_list = open(DEV_LIST_LOC, 'w')
 dev_list.write(str(nmap_dev_list).replace('\'', '\"').replace('None', '\"None\"')) #for JSON compatibility
 dev_list.close()
+
+f = open(DEV_LIST_LOC, 'r')
+devices = json.load(f)
+f.close()
+
+id1 = identifier(devices, VENDORS)
+print(id1.iotid())
