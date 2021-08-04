12b69a67a1948b7ad5fbc80d31b741ca2b8db6ef
Yichen Sun <50383075+Tigersteve123@users.noreply.github.com>
2021-08-04 10:39:53 -0400

    Update CLI

--- lib/cli.py
+++ lib/cli.py
@@ -18,14 +18,33 @@ sc = scanner(HOST)
 id1 = identifier(devices, VENDORS)
 
 def nmap_list_scan():
-    sc.nmap_list_scan()
+    nmap_dev_list = sc.nmap_list_scan()
+    dev_list = open(DEV_LIST_LOC, 'w')
+    dev_list.write(str(nmap_dev_list).replace('\'', '\"').replace('None', '\"None\"')) #for JSON compatibility
+    dev_list.close()
     
 def iotid():
-    id1.iotid()
+    iot_devs = id1.iotid()
+    print([i['ip'] for i in iot_devs])
+    return [i['ip'] for i in iot_devs]
+
+def lsdev():
+    for key, value in devices.items():
+        try:
+            print('ip: ' + key + '; vendor: ' + value['macaddress']['vendor'])
+        except: pass
 
 parser = argparse.ArgumentParser(description='Set up and configure iotfw', prog='iotfw', allow_abbrev=False)
 
-parser.add_argument('--scan', '-s', action='store_const', const=nmap_list_scan)
-parser.add_argument('--id', '-i', action='store_const', const=iotid)
+parser.add_argument('-s', '--scan', action='store_true', help='Scan network and write to file')
+parser.add_argument('-i', '--id', action='store_true', help='Identify IoT devices')
+parser.add_argument('-ld', '--lsdev', action='store_true', help='List all devices')
 
 args = parser.parse_args()
+
+if args.scan:
+    nmap_list_scan()
+if args.id:
+    iotid()
+if args.lsdev:
+    lsdev()
